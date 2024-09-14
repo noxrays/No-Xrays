acf40e331aa4b7006d84e026316277d2ff90c6d4
noxrays <elizabethdelrosario1@gmail.com>
2024-09-14 12:12:46 -0500

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,9 @@
+<script>
+    function showSidebar() {
+        document.querySelector('.sidebar').style.display = 'flex'; // Show the sidebar
+    }
+
+    function hideSidebar() {
+        document.querySelector('.sidebar').style.display = 'none'; // Hide the sidebar
+    }
+</script>
